e02d43b1f4b53f66be5c129846b10a384fceabae
FFFFFight <zhuao5043@163.com>
2021-07-08 17:29:43 +0800

    test commit to github

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def test_commit():
+    print('Done!')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
